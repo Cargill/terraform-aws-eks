24cf79c2412ee9b7f572483e87a68f645af7a936
seancurran157 <seancurran157@gmail.com>
2018-10-11 16:27:12 -0500

    Update cluster.tf

--- cluster.tf
+++ cluster.tf
@@ -69,21 +69,13 @@ resource "aws_iam_service_linked_role" "elasticloadbalancing" {
   aws_service_name = "elasticloadbalancing.amazonaws.com"
 }
 
-resource "aws_security_group" "cluster" {
-  name_prefix = "${var.cluster_name}"
-  description = "EKS cluster security group."
-  vpc_id      = "${var.vpc_id}"
-  tags        = "${merge(var.tags, map("Name", "${var.cluster_name}-eks_cluster_sg"))}"
-  count       = "${var.cluster_security_group_id == "" ? 1 : 0}"
-}
-
-resource "aws_security_group_rule" "cluster_egress_internet" {
-  description       = "Allow cluster egress access to the Internet."
-  protocol          = "-1"
-  security_group_id = "${aws_security_group.cluster.id}"
-  cidr_blocks       = ["0.0.0.0/0"]
-  from_port         = 0
-  to_port           = 0
-  type              = "egress"
-  count             = "${var.cluster_security_group_id == "" ? 1 : 0}"
+resource "aws_security_group_rule" "cluster_https_worker_ingress" {
+  description              = "Allow pods to communicate with the EKS cluster API."
+  protocol                 = "tcp"
+  security_group_id        = "${aws_security_group.cluster.id}"
+  source_security_group_id = "${local.worker_security_group_id}"
+  from_port                = 443
+  to_port                  = 443
+  type                     = "ingress"
+  count                    = "${var.cluster_security_group_id == "" ? 1 : 0}"
 }
